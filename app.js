f4ef2bea90ae522162369400e8e78f8921640326
SerhiiChynin <sergey.chinin@gmail.com>
2023-02-01 20:37:22 +0200

    hw node

--- app.js
+++ app.js
@@ -17,3 +17,15 @@ b();
 
 const c = [1, 2, 3, 4, 5, 6];
 console.log(c[3]);
+
+// Home work
+function a1(a, b) {
+    if (a > b) return a;
+    return b;
+}
+console.log(a1(10, 21));
+
+function a2(min, max) {
+     return Math.floor(Math.random() * (max - min)) + min;
+}
+console.log(a2(0,10));
