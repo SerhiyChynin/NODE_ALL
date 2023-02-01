d9710bf84669697c143a6b9a1a75d79d85443dd8
SerhiiChynin <sergey.chinin@gmail.com>
2023-02-01 20:30:46 +0200

    NODE

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log('Hello');
+
+let a;
+a = '9';
+console.log(a + 10);
+
+function test() {
+    console.log('1111111');
+}
+test();
+
+const b = () => {
+    console.log('Hello');
+}
+
+b();
+
+const c = [1, 2, 3, 4, 5, 6];
+console.log(c[3]);
