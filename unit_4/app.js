89ea6a969ee98b6ddeacea1fb25311231d1c1ca5
SerhiiChynin <sergey.chinin@gmail.com>
2023-05-31 11:53:41 +0300

    php storm

--- unit_4/app.js
+++ unit_4/app.js
@@ -1,10 +1,15 @@
 let a = 6;
 let b = 7;
-b = b * 2;
-a = a + b;
-console.log(a);
-console.log(b);
-console.log(a + b);
+
+function t() {
+    let a = 8;
+    let c = a + b;
+    // a = a + d;
+    console.log(a + c);
+
+}
+
+t();
 
 
 
