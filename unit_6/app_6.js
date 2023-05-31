b71f780bdf361ffca2d904406423022d32fc4a03
SerhiiChynin <sergey.chinin@gmail.com>
2023-05-31 16:13:09 +0300

    fs.read write

---
+++ unit_6/app_6.js
@@ -0,0 +1,24 @@
+const fs = require('fs'); //модуль файловой системы, подключаем, обязателен для чтения файлов
+const path = require('path'); //разширение для работы с путями
+//task 1 - read a file;
+//way 1 : асинхронный идет дальше выводит когда прочитает
+// fs.readFile('t2.txt', 'utf-8', (err, data)=>{
+//     console.log(data)
+// }) //параметры: путь, кодировка файла, асинк функция будет получать и обрабатывать рез
+//way 2 : синхронный ждем пока прочитает потом идем дальше
+// let text = fs.readFileSync('t1.txt', 'utf-8')
+// console.log(text);
+// console.log('==================');
+
+//read directore
+
+fs.readdir('one', (err, data) => {
+    console.log(data)
+    data.forEach(file => {
+        console.log(file + '\t\t' + path.extname(file) + '\t\t' + fs.statSync('one/' + file).size + ' b')
+    })
+})
+
+fs.writeFile('one/newfile.txt', 'better head home and working on it now', (err) => {
+    if(err) return console.log(err);
+})
