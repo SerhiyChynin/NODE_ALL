0eeca3f98cca8af95fe877bc58471af469ef331b
SerhiiChynin <sergey.chinin@gmail.com>
2023-06-11 17:22:16 +0300

    project upload files

--- unit_13_project/nodejs_unit_code_lesson_15/app_project/app_pro.js
+++ unit_13_project/nodejs_unit_code_lesson_15/app_project/app_pro.js
@@ -3,20 +3,20 @@ const fs = require('fs');
 const path = require('path');
 
 http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url);
     if (req.url == '/') {
         sendRes('index.html', 'text/html', res);
     } else if (/\/uploads\/[^\/]+$/.test(req.url) && req.method == 'POST') {
 
     } else {
-
+        sendRes(req.url, getContentType(req.url), res);
     }
 }).listen(3000, () => {
     console.log('server start 3000');
 });
 
 function sendRes(url, contentType, res) {
-    let file = path.join(__dirname+'/static/', url);
+    let file = path.join(__dirname + '/static/', url);
     fs.readFile(file, (err, content) => {
         if (err) {
             res.writeHead(404);
@@ -27,7 +27,22 @@ function sendRes(url, contentType, res) {
             res.writeHead(200, {'Content-Type': contentType});
             res.write(content);
             res.end();
-            console.log(200 `${file}`);
+            console.log(`res 200 ${file}`);
         }
     })
+}
+
+function getContentType(url) {
+    switch (path.extname(url)) {
+        case ".html":
+            return "text/html";
+        case ".css":
+            return "text/css";
+        case ".js":
+            return "text/javascript";
+        case ".json":
+            return "application/json";
+        default:
+            return "application/octate-stream";
+    }
 }
