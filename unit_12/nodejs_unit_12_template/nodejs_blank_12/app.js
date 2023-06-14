3d676880d74cba3a18e51042b1d9ab5f2c68f1a8
SerhiiChynin <sergey.chinin@gmail.com>
2023-06-14 10:28:36 +0300

    module hw random string

--- unit_12/nodejs_unit_12_template/nodejs_blank_12/app.js
+++ unit_12/nodejs_unit_12_template/nodejs_blank_12/app.js
@@ -2,13 +2,18 @@ const config = require('./config');
 const randomInt = require('./rand');
 const config2 = require('./config2');
 const indexArray = require('./indexArray');
-// console.log(config);
+const index = require('./hash/index');
+
+let a = [
+    {id: 24, name: 'Test', age: 23},
+    {id: 34, name: 'User', age: 33}
+];
+
+console.log(config);
 // console.log(randomInt(0,5));
 // console.log(config2);
-let a = [
-    { id : 24, name : 'Test', age : 23},
-    { id : 34, name : 'User', age : 33}
-];
-console.log(indexArray(a, 'name'))
+// console.log(indexArray(a, 'name'))
+// console.log(index(10))
+
 
 
--- unit_12/nodejs_unit_12_template/nodejs_blank_12/hash/index.js
+++ unit_12/nodejs_unit_12_template/nodejs_blank_12/hash/index.js
@@ -1,3 +1,8 @@
+const arr = require('./config');
 module.exports = function (a) {
-    return a;
+    let string = '';
+    for (let i = 0; i < a; i++) {
+        string += arr[Math.round(Math.random() * (arr.length - 1))];
+    }
+    return string;
 }
